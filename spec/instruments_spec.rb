54fd81e987bbf3b3fd09c6a859675cc408489fad
Brian Cobb <bcobb@uwalumni.com>
2011-04-18 12:58:57 -0500

    rewriting instruments spec

--- spec/instruments_spec.rb
+++ spec/instruments_spec.rb
@@ -1,62 +1,96 @@
 require 'spec_helper'
 
 describe Metrics::Instruments do
+
+  subject { Metrics::Instruments }
+
   before(:each) do
-    @instruments = Metrics::Instruments
-    @instruments.unregister_all
-    
-    @counter = Metrics::Instruments::Counter.new
-    @meter = Metrics::Instruments::Meter.new
-  end
-  
-  it "should initially have no instruments in its hash" do
-    @instruments.registered.should == {}
-  end
-  
-  it "should allow for registering a Counter instrument" do
-    Metrics::Instruments::Counter.stub!(:new).and_return @counter
-    @instruments.register(:counter, :test_counter).should == @counter
-    @instruments.registered.should == {:test_counter => @counter}
-  end
-  
-  it "should allow for registering a Meter instrument" do
-    Metrics::Instruments::Meter.stub!(:new).and_return @meter
-    @instruments.register(:meter, :test_meter).should == @meter
-    @instruments.registered.should == {:test_meter => @meter}
+    subject.unregister_all
   end
 
-  it "should allow for registering a Histogram instrument using uniform sampling" do
-    histogram = Metrics::Instruments::UniformHistogram.new
-    Metrics::Instruments::UniformHistogram.stub!(:new).and_return histogram
-    @instruments.register(:uniform_histogram, :test_histogram).should == histogram
-    @instruments.registered.should == {:test_histogram => histogram}
+  context 'initially' do
+
+    it 'should have no registered instruments' do
+      subject.registered.should be_empty
+    end
+
   end
 
-  it "should allow for registering a Histogram instrument using exponentially decaying sampling" do
-    histogram = Metrics::Instruments::ExponentialHistogram.new
-    Metrics::Instruments::ExponentialHistogram.stub!(:new).and_return histogram
-    @instruments.register(:exponential_histogram, :test_histogram).should == histogram
-    @instruments.registered.should == {:test_histogram => histogram}
+  describe '.register' do
+
+    Metrics::Instruments.types.each do |instrument, instrument_class|
+
+      context "given :#{instrument}, a name, and a block" do
+
+        let(:name) { :test }
+        let(:block) { Proc.new { :block } }
+
+        it "should return a new #{instrument_class}" do
+          subject.register(instrument, name, &block).
+            should be_a(instrument_class)
+        end
+
+        it "should register a new #{instrument_class} under the given name" do
+          test_instrument = double(instrument_class)
+          instrument_class.stub!(:new).and_return(test_instrument)
+
+          subject.register(instrument, name, &block)
+          subject.registered[name].should eql(test_instrument)
+        end
+        
+      end
+
+    end
+
   end
 
-  
-  it "should generate JSON correctly" do 
-    Metrics::Instruments::Meter.stub!(:new).and_return @meter
-    @instruments.register(:meter, :test_meter).should == @meter
-    @instruments.registered.should == {:test_meter => @meter}
-    
-    @instruments.to_json.should == "{\"test_meter\":\"{\\\"one_minute_rate\\\":0.0,\\\"five_minute_rate\\\":0.0,\\\"fifteen_minute_rate\\\":0.0}\"}"
+  describe '.unregister_all' do
+
+    before do
+      instrument = Metrics::Instruments.types.keys.first
+      subject.register(instrument, instrument) do
+        :block
+      end
+    end
+
+    it 'should clear the registered instruments' do
+      expect {
+        subject.unregister_all
+      }.to change(subject, :registered).to({})
+    end
+
   end
-  
-  it "should not allow for creating a gauge with no block" do 
-    lambda do
-      @instruments.register :gauge, :test_gauge
-    end.should raise_error(ArgumentError)
+
+  describe '.registered' do
+
+    let(:fake_counter) { double(Metrics::Instruments::Counter) }
+
+    before do
+      Metrics::Instruments::Counter.stub!(:new).and_return(fake_counter)
+      subject.register(:counter, :test_counter)
+    end
+
+    it 'should return a Hash of registered names and instruments' do
+      subject.registered.should eql({
+        :test_counter => fake_counter
+      })
+    end
+
   end
-  
-  it "should allow for creating a gauge with a block" do 
-    proc = Proc.new { "result" }
-    @instruments.register :gauge, :test_gauge, &proc
+
+  describe '.to_json' do
+
+    let(:registered) { double(:@registered) }
+
+    before do
+      subject.stub!(:registered).and_return(registered)
+    end
+
+    it 'should return .registered JSONified' do
+      registered.should_receive(:to_json)
+      subject.to_json
+    end
+
   end
-    
+
 end
