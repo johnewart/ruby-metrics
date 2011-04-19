8c74c74cc128765ea80f56f2b014dabb7fd592a0
Brian Cobb <bcobb@uwalumni.com>
2011-04-18 21:28:08 -0500

    adding #calibrate to Calibration

--- lib/ruby-metrics/instruments/calibration.rb
+++ lib/ruby-metrics/instruments/calibration.rb
@@ -8,6 +8,16 @@ module Metrics
         @duration_unit = @rate_unit = :seconds
       end
 
+      def calibrate(calibration)
+        [:duration_unit, :rate_unit].each do |parameter|
+          if calibration.respond_to?(parameter)
+            writer = parameter.to_s + '='
+            value = calibration.send(parameter)
+            self.send(writer, value)
+          end
+        end
+      end
+
     end
   end
 end
--- spec/instruments/calibration_spec.rb
+++ spec/instruments/calibration_spec.rb
@@ -9,4 +9,31 @@ describe Metrics::Instruments::Calibration do
 
   end
 
+  describe '#calibrate' do
+
+    context 'given another calibration' do
+
+      let(:calibration) { Metrics::Instruments::Calibration.new }
+
+      before(:all) do
+        calibration.duration_unit = :fake_units
+        calibration.rate_unit = :fake_units
+      end
+
+      it "sets its duration unit to the given calibration's duration unit" do
+        expect {
+          subject.calibrate(calibration)
+        }.to change(subject, :duration_unit).from(:seconds).to(:fake_units)
+      end
+
+      it "sets its rate unit to the given calibration's rate unit" do
+        expect {
+          subject.calibrate(calibration)
+        }.to change(subject, :rate_unit).from(:seconds).to(:fake_units)
+      end
+
+    end
+
+  end
+
 end
