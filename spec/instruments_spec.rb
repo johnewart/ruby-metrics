ebaa80bba0567b4b18932e5c7ab189e70f2f3547
Clayton Wheeler <cswheeler@gmail.com>
2011-06-08 12:38:29 -0700

    Added JSON tests for Gauge, Histogram, and Timer to cover the ActiveSupport issues.

--- spec/instruments_spec.rb
+++ spec/instruments_spec.rb
@@ -55,6 +55,22 @@ describe Metrics::Instruments do
     
     @instruments.to_json.should == %({"test_counter":0})
   end
+
+  it "should generate JSON for a Gauge correctly" do
+    val = 3
+    @instruments.register(:gauge, :test_gauge) { val }
+    @instruments.to_json.should == %({"test_gauge":3})
+  end
+  
+  it "should generate JSON for a Histogram correctly" do
+    @instruments.register(:exponential_histogram, :test_histogram)
+    @instruments.to_json.should == %({"test_histogram":{"min":0.0,"max":0.0,"mean":0.0,"variance":0.0,"percentiles":{"0.25":0.0,"0.5":0.0,"0.75":0.0,"0.95":0.0,"0.97":0.0,"0.98":0.0,"0.99":0.0}}})
+  end
+  
+  it "should generate JSON for a Timer correctly" do
+    @instruments.register(:timer, :test_timer)
+    @instruments.to_json.should == %({"test_timer":{"count":0,"rates":{"one_minute_rate":0.0,"five_minute_rate":0.0,"fifteen_minute_rate":0.0,"unit":"seconds"},"durations":{"min":0.0,"max":0.0,"mean":0.0,"percentiles":{"0.25":0.0,"0.5":0.0,"0.75":0.0,"0.95":0.0,"0.97":0.0,"0.98":0.0,"0.99":0.0},"unit":"seconds"}}})
+  end
   
   it "should not allow for creating a gauge with no block" do 
     lambda do
