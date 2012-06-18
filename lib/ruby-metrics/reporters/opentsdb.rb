ccb502655f9dd1359b8f67c152b5d7b862c3965f
John Ewart <john@unixninjas.org>
2012-06-18 08:04:09 -0700

    Adding OpenTSDB to the reporters

---
+++ lib/ruby-metrics/reporters/opentsdb.rb
@@ -0,0 +1,70 @@
+require 'ruby-metrics/version'
+require 'ruby-metrics'
+
+require 'opentsdb'
+
+module Metrics
+  module Reporters
+    class OpenTSDBReporter
+
+      attr_reader :hostname
+      attr_reader :port
+      attr_reader :client
+      attr_reader :tags
+
+      def initialize(options = {})
+        @hostname = options[:hostname]
+        @port = options[:port] 
+        @client = OpenTSDB::Client.new({:hostname => @hostname, :port => @port})
+        @tags = options[:tags] || {}
+      end
+
+      def send_data(data)
+        tsdb_data = {
+          :metric => data[:name],
+          :timestamp => Time.now.to_i,
+          :value => data[:value],
+          :tags => data[:tags].merge(@tags)
+        }
+        puts "Sending #{tsdb_data}"
+        @client.put(tsdb_data)
+      end
+
+      def report(agent)
+
+        agent.instruments.each do |name, instrument|
+          nothing_to_do = false
+          data =  { :name => name, :tags => { :units => instrument.units } }
+          case instrument
+            when Metrics::Instruments::Counter
+              value = instrument.to_i
+              data.merge! :value => value.to_i
+              send_data data
+            when Metrics::Instruments::Gauge
+              if instrument.get.is_a? Hash
+                instrument.get.each do |key, value|
+                  data.merge! :name => "#{name}.#{key}", :value => value
+                  send_data data
+                end
+              else 
+                data.merge! :value => instrument.get
+                send_data data
+              end
+            when Metrics::Instruments::Timer
+              [:count, :fifteen_minute_rate, :five_minute_rate, :one_minute_rate, :min, :max, :mean].each do |attribute|
+                data.merge!(:name => "#{name}.#{attribute}", :value => instrument.send(attribute))
+                send_data data
+              end
+            when Metrics::Instruments::Meter
+              [:count, :fifteen_minute_rate, :five_minute_rate, :one_minute_rate, :mean_rate].each do |attribute|
+                data.merge!(:name => "#{name}.#{attribute}", :value => instrument.send(attribute) )
+              end
+            else 
+              puts "Unhandled instrument"
+          end
+        end
+      end
+    end
+  end
+end
+
